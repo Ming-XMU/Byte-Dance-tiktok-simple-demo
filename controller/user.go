7abf9e577e5ebe66e2b5ea3b3e06547764bac84b
zym <zym_edu@163.com>
2022-05-14 17:23:53 +0800

    666

--- controller/user.go
+++ controller/user.go
@@ -57,24 +57,24 @@ func Register(c *gin.Context) {
 	}
 }
 
-func Login(c *gin.Context) {
-	username := c.Query("username")
-	password := c.Query("password")
-
-	token := username + password
-
-	if user, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 0},
-			UserId:   user.Id,
-			Token:    token,
-		})
-	} else {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-		})
-	}
-}
+//func Login(c *gin.Context) {
+//	//username := c.Query("username")
+//	//password := c.Query("password")
+//
+//	//token := username + password
+//
+//	if user, exist := usersLoginInfo[token]; exist {
+//		c.JSON(http.StatusOK, UserLoginResponse{
+//			Response: Response{StatusCode: 0},
+//			UserId:   user.Id,
+//			Token:    token,
+//		})
+//	} else {
+//		c.JSON(http.StatusOK, UserLoginResponse{
+//			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
+//		})
+//	}
+//}
 
 func UserInfo(c *gin.Context) {
 	token := c.Query("token")
